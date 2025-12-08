c75dccea1f952efb0ee2af66160d9425914c3a4a
LarsDiedrich22 <icebear.22@web.de>
2025-12-08 11:46:53 +0100

    Fix combine image path for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,7 @@ const restartButton = document.getElementById("restart-button") as HTMLButtonEle
 
 // Combine sprite
 const combineImage = new Image();
-combineImage.src = "/draufsicht-des-mähdreschers-133656179.jpg.png";
+combineImage.src = "draufsicht-des-mähdreschers-133656179.jpg.png";
 let combineImageReady = false;
 combineImage.onload = () => {
   combineImageReady = true;
